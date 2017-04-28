ecb4dccd01b0307a07b8484eb2b8123fb6e013c4
Kerry Buckley <kerryjbuckley@gmail.com>
2017-04-28 17:07:10 +0100

    Allocate a single job to the chef

--- lib/roster.rb
+++ lib/roster.rb
@@ -1,5 +1,6 @@
 Roster = Struct.new :chef_jobs, :assistant_jobs do
   def self.from_completed_jobs total_jobs, jobs_completed
-    new [], []
+    pending_jobs = (1..total_jobs).to_a - jobs_completed
+    new pending_jobs, []
   end
 end
--- spec/roster_spec.rb
+++ spec/roster_spec.rb
@@ -3,9 +3,26 @@ require "roster"
 describe Roster do
   describe ".from_completed_jobs" do
     context "when there are no jobs" do
+      subject { described_class.from_completed_jobs 0, [] }
+
       it "allocates no jobs to either the chef or assistant" do
-        expect(described_class.from_completed_jobs 0, [])
-          .to eq described_class.new [], []
+        expect(subject).to eq described_class.new [], []
+      end
+    end
+
+    context "when all jobs are completed" do
+      subject { described_class.from_completed_jobs 3, [3, 1, 2] }
+
+      it "allocates no jobs to either the chef or assistant" do
+        expect(subject).to eq described_class.new [], []
+      end
+    end
+
+    context "when there is one uncompleted job" do
+      subject { described_class.from_completed_jobs 3, [3, 1] }
+
+      it "allocates it to the chef" do
+        expect(subject).to eq described_class.new [2], []
       end
     end
   end
